1dd1a7a677319c45780ccf6c13fabbab11a8408e
FrederikOlesen <Frederik.o@mailme.dk>
2014-03-20 12:51:21 +0100

    Added string to main.

--- src/semesterproject/Semesterproject.java
+++ src/semesterproject/Semesterproject.java
@@ -16,7 +16,7 @@ public class Semesterproject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String test = "Hejsa";
     }
     
 }
