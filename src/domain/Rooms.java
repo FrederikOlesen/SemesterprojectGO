1d6a30a2e62813c62f9b79deb766efc8a1b99318
Uffe <Uffe@10.18.137.54>
2014-03-21 09:25:09 +0100

    Rooms created

--- src/domain/Rooms.java
+++ src/domain/Rooms.java
@@ -12,4 +12,20 @@ package domain;
  */
 public class Rooms {
     
+    private String roomType;
+    private int price;
+    private int roomNumber;
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+    
 }
