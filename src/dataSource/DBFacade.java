f0f21adb8888c9d6cf58424f1a734c737281cf16
majep113 <mads.jeppesen@live.dk>
2014-04-01 10:37:14 +0200

    Added getters and setters to rooms

--- src/dataSource/DBFacade.java
+++ src/dataSource/DBFacade.java
@@ -13,81 +13,104 @@ import java.util.ArrayList;
  *
  * @author frederikolesen
  */
-public class DBFacade {
+public class DBFacade
+{
 
+    // objects used in class
     private UOWPBook uowb;
     private UOWPBook uowc;
     private Connection con;
+    // Variables used in class
     private int nextResNr;
     private int nextCustomerID;
 
-    //=====	Singleton
+    // Singleton
     private static DBFacade instance;
 
-    public DBFacade() {
+    public DBFacade()
+    {
         con = new DBConnector().getConnection();  // the connection will be released upon program 
     }
 
-    public static DBFacade getInstance() {
-        if (instance == null) {
+    public static DBFacade getInstance()
+    {
+        if (instance == null)
+        {
             instance = new DBFacade();
         }
         return instance;
     }
 
-    public int getNextResnr() {
+    // Method to get next reservation number from database
+    public int getNextResnr()
+    {
 
         nextResNr = new BookingMapper().getNextResNumber(con);
         return nextResNr;
     }
 
-    public int getNextCustomerID() {
+// Method to get next customer ID from database
+    public int getNextCustomerID()
+    {
         nextCustomerID = new BookingMapper().getNextCustomerID(con);
         return nextCustomerID;
     }
 
-    //=====	Methods to register changes	in UnitOfWork  
-    public void registerNewBooking(Booking b) {
-        if (uowb != null) {
+    // Methods to register changes
+    public void registerNewBooking(Booking b)
+    {
+        if (uowb != null)
+        {
             uowb.registerNewBooking(b);
         }
     }
 
-    public void registerDirtyBooking(Booking b) {
-        if (uowb != null) {
+    public void registerDirtyBooking(Booking b)
+    {
+        if (uowb != null)
+        {
             uowb.registerDirtyBooking(b);
         }
     }
 
-    public void registerNewCustomer(Customer c) {
-        if (uowc != null) {
+    public void registerNewCustomer(Customer c)
+    {
+        if (uowc != null)
+        {
             uowc.registerNewCustomers(c);
         }
     }
 
-    public void registerDirtyCustomer(Customer c) {
-        if (uowc != null) {
+    public void registerDirtyCustomer(Customer c)
+    {
+        if (uowc != null)
+        {
             uowc.registerDirtyCustomers(c);
         }
     }
 
-    //=== Methods to handle business transactions
-    //=====	Ignore changes after last commit
-    public void startNewBusinessTransactionBook() {
+    // Methods to handle business transactions
+    public void startNewBusinessTransactionBook()
+    {
         uowb = new UOWPBook();
     }
 
-    public void startNewBusinessTransactionCus() {
+    public void startNewBusinessTransactionCus()
+    {
         uowc = new UOWPBook();
     }
 
-    //=====	Save all changes
-    public boolean commitBusinessTransactionBooking() {
+    // Methods to save transactions to database
+    public boolean commitBusinessTransactionBooking()
+    {
         boolean status = false;
-        if (uowb != null) {
-            try {
+        if (uowb != null)
+        {
+            try
+            {
                 status = uowb.commit(con);
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 System.out.println("Fail in DBFacade - commitBusinessTransaction");
                 System.err.println(e);
             }
@@ -96,14 +119,18 @@ public class DBFacade {
         return status;
     }
 
-    public boolean commitBusinessTransactionCustomer() {
+    public boolean commitBusinessTransactionCustomer()
+    {
         boolean status = false;
         System.out.println("CommitBTC out if");
-        if (uowc != null) {
+        if (uowc != null)
+        {
             System.out.println("CommitBTC in if");
-            try {
+            try
+            {
                 status = uowc.commitCustomers(con);
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 System.out.println("Fail in DBFacade - commitBusinessTransaction");
                 System.err.println(e);
             }
@@ -112,18 +139,23 @@ public class DBFacade {
         return status;
     }
 
-    public ArrayList getCustomer(String lname) {
+    // Methods to retrieve data from database
+    public ArrayList getCustomer(String lname)
+    {
         ArrayList c = null;
         c = new BookingMapper().getCustomer(lname, con);
         return c;
     }
-    public ArrayList getCustomerID(String customerID) {
+
+    public ArrayList getCustomerID(String customerID)
+    {
         ArrayList c = null;
         c = new BookingMapper().getCustomerID(customerID, con);
         return c;
     }
-    
-    public ArrayList getBookingList(String arrival, String departure) {
+
+    public ArrayList getBookingList(String arrival, String departure)
+    {
         ArrayList b = null;
         b = new BookingMapper().getBookingList(arrival, departure, con);
         return b;
--- src/dataSource/UOWPBook.java
+++ src/dataSource/UOWPBook.java
@@ -5,8 +5,10 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class UOWPBook {
+public class UOWPBook
+{
 
+    // ArrayLists used to keep track of business transactions
     private final ArrayList<Booking> modifiedBooking;
     private final ArrayList<Booking> deleteBooking;
     private final ArrayList<Booking> newBooking;
@@ -14,8 +16,9 @@ public class UOWPBook {
     private final ArrayList<Customer> deleteCustomers;
     private final ArrayList<Customer> newCustomers;
 
-    // private ArrayList<Customers> newCustomers;
-    public UOWPBook() {
+    // Constructor
+    public UOWPBook()
+    {
         newBooking = new ArrayList<>(); // will never exceed 1 element
         modifiedBooking = new ArrayList<>(); // will never exceed 1 element
         deleteBooking = new ArrayList<>();
@@ -24,105 +27,132 @@ public class UOWPBook {
         modifiedCustomers = new ArrayList<>();
 
     }
-    //====== Methods to register changes ==========================
 
-    public void registerNewBooking(Booking b) {
+    // Register booking method, keeps track of new bookings
+    public void registerNewBooking(Booking b)
+    {
         if (!newBooking.contains(b) && // if not allready registered in any list
-                !modifiedBooking.contains(b)) {
+                !modifiedBooking.contains(b))
+        {
             newBooking.add(b);
         }
     }
 
-    public void registerDirtyBooking(Booking b) {
+    // Register dirty booking method, keeps track of modefied bookings
+    public void registerDirtyBooking(Booking b)
+    {
         if (!newBooking.contains(b) && // if not allready registered in any list
-                !modifiedBooking.contains(b)) {
+                !modifiedBooking.contains(b))
+        {
             modifiedBooking.add(b);
         }
     }
 
-    public void registerNewCustomers(Customer c) {
+    // Register customer method, keeps track of new customers
+    public void registerNewCustomers(Customer c)
+    {
         if (!newCustomers.contains(c) && // if not allready registered in any list
-                !modifiedCustomers.contains(c)) {
+                !modifiedCustomers.contains(c))
+        {
             newCustomers.add(c);
         }
     }
 
-    public void registerDirtyCustomers(Customer c) {
+// Register dirty customer method, keeps track of modified customers
+    public void registerDirtyCustomers(Customer c)
+    {
         if (!newCustomers.contains(c) && // if not allready registered in any list
-                !modifiedCustomers.contains(c)) {
+                !modifiedCustomers.contains(c))
+        {
             modifiedCustomers.add(c);
         }
     }
 
-    //====== Method to save changes to DB ===============================
-    public boolean commit(Connection conn) throws SQLException {
+    // Method to save changes to database
+    public boolean commit(Connection conn) throws SQLException
+    {
         boolean status = true;
-        try {
-            //=== system transaction - start
+        try
+        {
+// Start of system transaction
             conn.setAutoCommit(false);
             BookingMapper bm = new BookingMapper();
             status = status && bm.addNewBooking(newBooking, conn);
 //            status = status && bm.updateBooking(modifiedBooking, conn);
 //            status = status && bm.deleteBooking(deleteBooking, conn);
-            if (!status) {
+            if (!status)
+            {
 
                 throw new Exception("Business Transaction aborted");
             }
-            //=== system transaction - end with success
+            // System transaction ends with success
             conn.commit();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             System.out.println("fail in UnitOfWork - commit()");
             System.err.println(e);
-            //=== system transaction - end with roll back
+            // System transaction fails, rollsback
             conn.rollback();
             status = false;// rettelse
         }
         return status;
     }
 
-    public boolean commitCustomers(Connection conn) throws SQLException {
+    // Method to save changes to database
+    public boolean commitCustomers(Connection conn) throws SQLException
+    {
         boolean status = true;
-        try {
-            //=== system transaction - start
+        try
+        {
+            // Start of system transaction
             conn.setAutoCommit(false);
             BookingMapper bm = new BookingMapper();
             System.out.println("CommitCustomer");
             status = status && bm.addNewCustomer(newCustomers, conn);
 //            status = status && bm.updateBooking(modifiedBooking, conn);
 //            status = status && bm.deleteBooking(deleteBooking, conn);
-            if (!status) {
+            if (!status)
+            {
                 throw new Exception("Business Transaction aborted");
             }
-            //=== system transaction - end with success
+            // System transaction ends with success
             conn.commit();
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             System.out.println("fail in UnitOfWork - commit()");
             System.err.println(e);
-            //=== system transaction - end with roll back
+            // System transaction fails, rollsback
             conn.rollback();
             status = false;// rettelse
         }
         return status;
     }
 
-//    ====== Methods to read from DB ===================================================
-    public ArrayList getCustomers(String lname, Connection con) {
+// Method to read from database
+    public ArrayList getCustomers(String lname, Connection con)
+    {
         ArrayList c = null;;
-        try {
+        try
+        {
             c = new BookingMapper().getCustomer(lname, con);
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             System.out.println("fail in UnitOfWork - getOrder()");
             System.err.println(e);
         }
         return c;
 
     }
-    
-    public ArrayList getBookingList(String arrival, String departure, Connection con) {
+
+    // Method to read from database
+    public ArrayList getBookingList(String arrival, String departure, Connection con)
+    {
         ArrayList b = null;
-        try {
+        try
+        {
             b = new BookingMapper().getBookingList(arrival, departure, con);
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             System.out.println("fail in UnitOfWork - getBookingList");
             System.err.println(e);
         }
@@ -130,9 +160,11 @@ public class UOWPBook {
 
     }
 
-    public void registerDeletedBooking(Booking b) {
+    public void registerDeletedBooking(Booking b)
+    {
         if (!newBooking.contains(b) && // if not allready registered in any list
-                !deleteBooking.contains(b)) {
+                !deleteBooking.contains(b))
+        {
             modifiedBooking.add(b);
         }
     }
